0e1847341ce8494566eacb8829e1403b3b69eaf3
杨青松 <1260013987@qq.com>
2018-08-04 15:44:38 +0800

    添加了清除，保存按钮

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ var actions = document.getElementById('actions');
 var pen = document.getElementById('pen');
 var eraser = document.getElementById('eraser');
 var color = document.getElementsByClassName('colors')[0];
+var clera = document.getElementById("clear");
+var download = document.getElementById("download");
+var lineWidth = 5;
 //自动设置高宽
 autoSetCanvasSize(xx);
 //监听鼠标事件
@@ -12,32 +15,52 @@ ListenToUser(xx);
 /**********************************************************************/
 //橡皮状态
 var usingEraser = false;
+//改变画笔粗细
+thin.onclick = function () {
+  lineWidth = 5;
+}
+thick.onclick = function () {
+  lineWidth = 10;
+}
 //循环绑定事件
-  for(var i=0;i<color.children.length;i++){
-    color.children[i].onclick = function(e){
-      for(var j=0;j<color.children.length;j++){
-     color.children[j].classList.remove('active');
+for (var i = 0; i < color.children.length; i++) {
+  color.children[i].onclick = function (e) {
+    for (var j = 0; j < color.children.length; j++) {
+      color.children[j].classList.remove('active');
     }
-    var penColor  = this.className;
+    var penColor = this.className;
     this.classList.add('active');
     context.strokeStyle = penColor;
     context.fillStyle = penColor;
     console.log(penColor);
   }
-  }
-eraser.onclick = function(){
+}
+eraser.onclick = function () {
   usingEraser = true;
   eraser.classList.add('active');
   pen.classList.remove('active');
-  console.log(usingEraser);
+  clera.classList.remove('active');
 
 }
-pen.onclick = function(){
+pen.onclick = function () {
   usingEraser = false;
   pen.classList.add('active');
   eraser.classList.remove('active');
-  console.log(usingEraser);
+  clera.classList.remove('active');
 }
+clera.onclick = function () {
+  clera.classList.add('active');
+  context.clearRect(0, 0, xx.width, xx.width);
+  clera.classList.remove('active');
+}
+download.onclick = function () {
+  var url = xx.toDataURL('image/png/jpg');
+   var a = document.createElement('a');
+   a.href = url;
+   a.download = "canvas图片";
+   a.target = "_blank";
+   a.click();
+  }
 /**********************************************************************/
 //自动设置宽高函数
 function autoSetCanvasSize(canvas) {
@@ -58,7 +81,7 @@ function autoSetCanvasSize(canvas) {
 //监听用户动作
 function ListenToUser(canvas) {
   //画笔状态
-var paining = false;
+  var paining = false;
   var last = {
     x: undefined,
     y: undefined
@@ -72,7 +95,7 @@ var paining = false;
       if (usingEraser) {
         context.clearRect(x - 15, y - 15, 30, 30);
       } else {
-        huayuan(x,y,2);
+        huayuan(x, y, 2);
         //记录鼠标按下的坐标
         last.x = x;
         last.y = y;
@@ -136,15 +159,17 @@ var paining = false;
 
     };
   }
+
   function huayuan(x, y, rudius) {
     context.beginPath();
+    context.lineWidth = lineWidth;
     context.arc(x, y, rudius, Math.PI * 2, false);
     context.fill();
   }
 
   function huaxian(x1, y1, x2, y2) {
     context.beginPath();
-    context.lineWidth = 4;
+    context.lineWidth = lineWidth;
     context.moveTo(x1, y1); //起点
     context.lineTo(x2, y2); //终点
     context.stroke();
